b2b49b59eee6942a4adc4b9516437b6934b19e73
Ervinas Mozzuchinas <ervimoz05@gmail.com>
2026-07-06 14:54:59 +0300

    firewall functions that will be used, just declaration

--- include/packet_filter.h
+++ include/packet_filter.h
@@ -7,6 +7,7 @@
 
 typedef enum {RULE_ALLOW, RULE_DENY} rule_action_t;
 
+# Structure that saves one firewall rule
 typedef struct {
     uint32_t src_ip;
     uint32_t dst_ip;
@@ -15,8 +16,14 @@ typedef struct {
     rule_action_t action;
 } fw_rule_t;
 
+# Structure that saves how many rules are in the firewall togerher with the default action
 typedef struct {
     fw_rule_t rules[MAX_RULES];
     int count;
     rule_action_t default_action;
 } firewall_t;
+
+# Firewall main functions that will be used
+void fw_init(firewall_t *fw, rule_action_t default_action);
+int fw_add_rule(firewall_t *fw, const char *cidr, uint16_t port, rule_action_t action);
+int fw_check(const firewall_t *fw, const unsigned char *packet, int len);
